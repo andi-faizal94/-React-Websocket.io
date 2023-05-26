e9b9e40aed004e218ed57ae19bd5311bf917624d
Andi Faizal <andifaizal647@gmail.com>
2023-05-27 04:56:51 +0700

    change base code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,51 @@
+import { useEffect, useState } from 'react';
 import io from 'socket.io-client';
 import './App.css';
-import { useEffect } from 'react';
 
 const socket = io.connect('http://localhost:3001');
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [messageReceived, setMessageReceived] = useState('');
+
   const handleMessage = (e) => {
     e.preventDefault();
-    socket.emit('send_message', { message: 'hello' });
+    socket.emit('send_message', { message: message });
+    setMessage('');
   };
 
   useEffect(() => {
     socket.on('receive_message', (data) => {
-      alert(data.message);
+      setMessageReceived(data?.message);
     });
+    return () => {
+      socket.off('receive_message');
+    };
   }, []);
   return (
-    <div className='min-h-screen flex justify-center items-center'>
-      <form>
-        <label htmlFor='message'></label>
-        <input
-          className='px-2 py-2 outline outline-slate-50 border border-neutral-200 rounded-full mb-3'
-          type='text'
-          id='message'
-          name='message'
-          placeholder='Input message in here'
-        />
-        <br />
-        <button
-          type='submit'
-          className='px-2 py-1 bg-gray-500 text-white rounded-full'
-          onClick={handleMessage}
-        >
-          Send Message
-        </button>
-      </form>
+    <div className='min-h-screen '>
+      <div>
+        <form className='flex justify-center items-center min-h-[100px]'>
+          <label htmlFor='message'></label>
+          <input
+            className='px-2 py-2 outline outline-slate-50 border border-neutral-200 rounded-full mb-3'
+            type='text'
+            id='message'
+            name='message'
+            placeholder='Input message in here'
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+          <button
+            type='submit'
+            className='px-4 py-2 bg-gray-500 text-white rounded-full'
+            onClick={handleMessage}
+          >
+            Send Message
+          </button>
+        </form>
+        <h1> person: {messageReceived}</h1>
+      </div>
     </div>
   );
 }
